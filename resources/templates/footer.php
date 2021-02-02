514d1a0949da8345568ebbd1d6bed0897a143139
Daniel Broadhurst <daniel@beedesignstudio.co.uk>
2021-02-02 19:01:23 +0000

    remove key

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,76 +1,79 @@
 <footer id="footer">
-    <div class="grid-container">
-        <div class="grid-x grid-padding-x">
-            <div class="large-4 medium-4 cell">
-                <h2>Contact Info</h2>
-                <h3>S & S Aerials Ltd.</h3>
-                <ul class="lists">
-                    <li><i class="fas fa-map-marker-alt"></i> - Great Stone Rd, Stretford, Manchester, M16 0HD</li>
-                    <li><iframe style="border:0; width:100%; height:135px;" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ0VlV3uCte0gRJ9T6k10O_c4&key=AIzaSyDS3YXe3OWT85q0KnlBxm9CGGMUEWpx4WI" allowfullscreen></iframe></li>
-                    <li><a href="tel:07768600180"><i class="fas fa-phone-alt"></i> - Call Us Now</a></li>
-                    <li><a href="mailto:enquiries@ssaerials.co.uk"><i class="far fa-envelope"></i> - Email Us</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>review-us"><i class="fas fa-pen"></i> - Submit a Review</a></li>
-                </ul>
-            </div>
-            <div class="large-4 medium-4 cell">
-                <h3>Site Map</h3>
-                <ul class="lists">
-                    <li><a href="<?php echo BASE_URL; ?>">Home</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>about">About</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>services">Services</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>portfolio">Portfolio</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>contact">Contact</a></li>
-                </ul>
-                <h3>Our Services</h3>
-                <ul class="lists">
-                    <li><a href="<?php echo BASE_URL; ?>service/tv-aerial-Installation">TV Aerial Installation</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>service/satellite-installation">Satellite Installation</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>service/tv-satellite-setup">TV & Satellite Setup</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>service/aerial-storm-damage-repair">Aerial Storm Damage Repair</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>service/tv-wall-mounting">TV Wall Mounting</a></li>
-                </ul>
-            </div>
-            <div class="large-4 medium-4 cell">
-                <h3>Areas We Cover.</h3>
-                <div class="multiple-lists">
-                    <ul class="lists">
-                        <li>Manchester</li>
-                        <li>Oldham</li>
-                        <li>Stockport</li>
-                        <li>Didsbury</li>
-                        <li>Chorlton</li>
-                    </ul>
-                    <ul class="lists">
-                        <li>Bolton</li>
-                        <li>Ashton</li>
-                        <li>Levenshulme</li>
-                        <li>Wilmslow</li>
-                        <li>Moston</li>
-                    </ul>
-                    <ul class="lists">
-                        <li>Altrincham</li>
-                        <li>Greater Manchester</li>
-                        <li>Swinton</li>
-                        <li>Trafford</li>
-                        <li>Salford</li>
-                    </ul>
-                </div>
-                <h3>Product Installation.</h3>
-                <ul class="lists">
-                    <li>Freeview</li>
-                    <li>FreeSat</li>
-                    <li>Sky</li>
-                    <li>Now TV</li>
-                </ul>
-            </div>
+  <div class="grid-container">
+    <div class="grid-x grid-padding-x">
+      <div class="large-4 medium-4 cell">
+        <h2>Contact Info</h2>
+        <h3>S & S Aerials Ltd.</h3>
+        <ul class="lists">
+          <li><i class="fas fa-map-marker-alt"></i> - Great Stone Rd, Stretford, Manchester, M16 0HD</li>
+          <li><iframe style="border:0; width:100%; height:135px;"
+              src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ0VlV3uCte0gRJ9T6k10O_c4&key=GOOGLEAPIKEY"
+              allowfullscreen></iframe></li>
+          <li><a href="tel:07768600180"><i class="fas fa-phone-alt"></i> - Call Us Now</a></li>
+          <li><a href="mailto:enquiries@ssaerials.co.uk"><i class="far fa-envelope"></i> - Email Us</a></li>
+          <li><a href="<?php echo BASE_URL; ?>review-us"><i class="fas fa-pen"></i> - Submit a Review</a></li>
+        </ul>
+      </div>
+      <div class="large-4 medium-4 cell">
+        <h3>Site Map</h3>
+        <ul class="lists">
+          <li><a href="<?php echo BASE_URL; ?>">Home</a></li>
+          <li><a href="<?php echo BASE_URL; ?>about">About</a></li>
+          <li><a href="<?php echo BASE_URL; ?>services">Services</a></li>
+          <li><a href="<?php echo BASE_URL; ?>portfolio">Portfolio</a></li>
+          <li><a href="<?php echo BASE_URL; ?>contact">Contact</a></li>
+        </ul>
+        <h3>Our Services</h3>
+        <ul class="lists">
+          <li><a href="<?php echo BASE_URL; ?>service/tv-aerial-Installation">TV Aerial Installation</a></li>
+          <li><a href="<?php echo BASE_URL; ?>service/satellite-installation">Satellite Installation</a></li>
+          <li><a href="<?php echo BASE_URL; ?>service/tv-satellite-setup">TV & Satellite Setup</a></li>
+          <li><a href="<?php echo BASE_URL; ?>service/aerial-storm-damage-repair">Aerial Storm Damage Repair</a></li>
+          <li><a href="<?php echo BASE_URL; ?>service/tv-wall-mounting">TV Wall Mounting</a></li>
+        </ul>
+      </div>
+      <div class="large-4 medium-4 cell">
+        <h3>Areas We Cover.</h3>
+        <div class="multiple-lists">
+          <ul class="lists">
+            <li>Manchester</li>
+            <li>Oldham</li>
+            <li>Stockport</li>
+            <li>Didsbury</li>
+            <li>Chorlton</li>
+          </ul>
+          <ul class="lists">
+            <li>Bolton</li>
+            <li>Ashton</li>
+            <li>Levenshulme</li>
+            <li>Wilmslow</li>
+            <li>Moston</li>
+          </ul>
+          <ul class="lists">
+            <li>Altrincham</li>
+            <li>Greater Manchester</li>
+            <li>Swinton</li>
+            <li>Trafford</li>
+            <li>Salford</li>
+          </ul>
         </div>
+        <h3>Product Installation.</h3>
+        <ul class="lists">
+          <li>Freeview</li>
+          <li>FreeSat</li>
+          <li>Sky</li>
+          <li>Now TV</li>
+        </ul>
+      </div>
     </div>
+  </div>
 </footer>
 
 <div class="grid-x grid-padding-x" id="bds-bar">
-    <div class="large-4 cell text-right mobile-call">
-        <p>Designed by <a href="https://beedesignstudio.co.uk" title="Designed by Bee Design Studio">Bee Design Studio</a></p>
-    </div>
+  <div class="large-4 cell text-right mobile-call">
+    <p>Designed by <a href="https://beedesignstudio.co.uk" title="Designed by Bee Design Studio">Bee Design Studio</a>
+    </p>
+  </div>
 </div>
 
 <script src="<?php echo BASE_URL; ?>js/vendor/jquery.js"></script>
